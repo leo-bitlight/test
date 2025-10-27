7fa708aa1c15ec3a906ea229968949d26dde30d1
Leo <afuafu@bitlightlabs.com>
2025-10-27 11:24:29 +0800

    chang js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+};
+
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import AccountProvider from "@/components/AccountProvider";
 import Header from '../components/Header'
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 
 export default function RootLayout({
@@ -26,7 +26,8 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
+      <body >
         <AccountProvider>
         <Header />
         {children}
