7610abad13fa763a36a68374a4148993ef45f06b
Leo <afuafu@bitlightlabs.com>
2025-10-28 16:49:45 +0800

    change js

--- src/components/AccountProvider.tsx
+++ src/components/AccountProvider.tsx
@@ -1,17 +1,22 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AccountContext from "@/context/AccountContext"
 
 export default function AccountProvider(props: any) {
-  const cached = localStorage.getItem('address')
-
-  const [address, setAddress] = useState(cached || '')
+  const [address, setAddress] = useState('')
 
   const setAddressAndCache = (addr: string) => {
     setAddress(addr)
     localStorage.setItem('address', addr)
   }
+
+  useEffect(() => {
+    const cached = localStorage.getItem('address')
+    if (cached) {
+      setAddress(cached)
+    }
+  }, [])
 
   return (
     <AccountContext.Provider value={{ address, setAddress: setAddressAndCache }}>{props.children}</AccountContext.Provider>
